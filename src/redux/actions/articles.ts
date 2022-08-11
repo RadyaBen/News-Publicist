1286c27ca44160352eb07108ad9cca7c6869f88c
RadyaBen <lizoguben.radya@gmail.com>
2022-08-11 17:51:50 +0300

    Hide the API key

--- src/redux/actions/articles.ts
+++ src/redux/actions/articles.ts
@@ -8,10 +8,10 @@ export const fetchArticles = () => async (dispatch: Dispatch<ArticlesAction>) =>
 	try {
 		dispatch({ type: ArticlesActionTypes.ARTICLES_FETCHING });
 
-		const _apiBase = 'https://newsapi.org/v2/everything?domains=wsj.com&language=en&';
-		const _apiKey = 'apiKey=59ec6f4f27c643c7b16a2a0ab98c629d';
+		const _apiBase = 'https://newsapi.org/v2/everything?domains=wsj.com&language=en&apiKey=';
+		const _API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 
-		const response = await axios.get(`${_apiBase + _apiKey}`);
+		const response = await axios.get(`${_apiBase + _API_KEY!}`);
 
 		const _transformArticles = (): IArticle[] => {
 			return response.data.articles.map((article: IArticle) => {
